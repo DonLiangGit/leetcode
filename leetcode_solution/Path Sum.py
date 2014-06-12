01e689f1058ac2b3f18882f924687b1716a06b69
Don Liang <duolingl@gmail.com>
2014-06-12 15:59:54 -0700

    Addig Path Sum

---
+++ leetcode_solution/Path Sum.py
@@ -0,0 +1,23 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @param sum, an integer
+    # @return a boolean
+    def hasPathSum(self, root, sum):
+    	# case 1: if the tree is empty, return False
+        if root == None:
+        	return False
+        # case 2: if the tree only has one root node, compare the valut to target
+        if root.left == None and root.right == None:
+        	return root.val == sum
+
+        # case 3: otherwise, return OR
+        # Called Logical OR Operator. If any of the two operands are non zero then then condition becomes true.
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+
