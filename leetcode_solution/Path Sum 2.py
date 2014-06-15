2f19122d3c9bc6818c8258b2fb11d4459ea7e199
Don Liang <duolingl@gmail.com>
2014-06-15 11:22:03 -0700

    Path Sum 2

--- leetcode_solution/Path Sum 2.py
+++ leetcode_solution/Path Sum 2.py
@@ -4,10 +4,25 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+# Memory Limit Exceeded: Solution/append wrong list,arguement is wrong
 class Solution:
     # @param root, a tree node
     # @param sum, an integer
     # @return a list of lists of integers
     def pathSum(self, root, sum):
-        
+    	if root == None:
+    		return []
+    	Solution.sum = sum
+    	Solution.result = []
+    	self.subpathSum(root,[root.val],root.val)
+    	return Solution.result
+
+    def subpathSum(self, node, temList, temSum):
+    	if node.left == None and node.right == None:
+    		if temSum == Solution.sum:
+    			Solution.result.append(temList)
+    		return
+    	if node.left != None:
+    		self.subpathSum (node.left, temList+[node.left.val], temSum+node.left.val)
+    	if node.right != None:
+    		self.subpathSum (node.right, temList+[node.right.val], temSum+node.right.val)
