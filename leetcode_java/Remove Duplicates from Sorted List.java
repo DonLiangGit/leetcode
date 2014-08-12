cfe24444646f553a2225a2a19a6c5347e2ba4459
Don Liang <duolingl@gmail.com>
2014-08-12 12:52:37 -0700

    first commit

---
+++ leetcode_java/Remove Duplicates from Sorted List.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// public class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+        
+//     }
+// }
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+		if (head == null || head.next == null) {
+			return head;
+		} else {
+			ListNode headForward = head;
+			ListNode compareNode = head.next;
+
+			while (compareNode != null) {
+				if (compareNode.val == headForward.val) {
+					headForward.next = compareNode;
+					compareNode = compareNode.next;
+				} else {
+					headForward =  compareNode;
+					compareNode = compareNode.next;
+				}
+			}
+			return head;
+		}
+    }
+}
